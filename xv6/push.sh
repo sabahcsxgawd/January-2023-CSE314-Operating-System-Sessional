5aac7c8dd6068d4dc637ae751fd44d2b2711ed95
sabahcsxgawd <sabah72224@gmail.com>
2023-07-23 19:49:20 +0600

    ansan

--- xv6/push.sh
+++ xv6/push.sh
@@ -1,4 +1,4 @@
-cd /media/sabah/269CFD019CFCCC75/Level\ 3\ Term\ 2/Sessional/January\ 2023\ CSE314\ Operating\ System\ Sessional
+cd "/media/sabah/269CFD019CFCCC75/Level 3 Term 2/Sessional/January 2023 CSE314 Operating System Sessional"
 git add --all
 git rm -rf --cached xv6/xv6-riscv
 git commit -m "ansan"
