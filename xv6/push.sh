8f93168bb1968bd2686a35d6fcf1419f49bcc94e
sabahcsxgawd <sabah72224@gmail.com>
2023-07-23 19:40:49 +0600

    ansan

---
+++ xv6/push.sh
@@ -0,0 +1,5 @@
+cd /media/sabah/269CFD019CFCCC75/Level\ 3\ Term\ 2/Sessional/January\ 2023\ CSE314\ Operating\ System\ Sessional
+git add --all
+git rm -rf --cached xv6/xv6-riscv
+git commit -m "ansan"
+git push -u origin master
--- xv6/run.sh
+++ xv6/run.sh
@@ -1,4 +1,6 @@
-cd ./xv6-riscv/
+cd /media/sabah/269CFD019CFCCC75/Level 3 Term 2/Sessional/January 2023 CSE314 Operating System Sessional/xv6/xv6-riscv
+git add --all
+git diff HEAD >> ../../Offline\ 3/1905118.patch
 make clean
 clear
 make qemu
