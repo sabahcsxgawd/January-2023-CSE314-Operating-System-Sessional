9b1abb391a3e689a97e3626b7da218b66deb00d9
sabahcsxgawd <sabah72224@gmail.com>
2023-08-24 23:49:55 +0600

    uvmmirror

--- xv6/push.sh
+++ xv6/push.sh
@@ -2,5 +2,5 @@
 cd "/media/sabah/269CFD019CFCCC75/Level 3 Term 2/Sessional/January 2023 CSE314 Operating System Sessional"
 git add --all
 git rm -rf --cached xv6/xv6-riscv
-git commit -m "ansan"
+git commit -m "$1"
 git push -u origin master
