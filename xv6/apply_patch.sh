3fbc0bb111742cf493396e91018b9386f7c2d17a
sabahcsxgawd <sabah72224@gmail.com>
2023-08-24 23:48:18 +0600

    ansan

--- xv6/apply_patch.sh
+++ xv6/apply_patch.sh
@@ -2,4 +2,4 @@
 cd "/media/sabah/269CFD019CFCCC75/Level 3 Term 2/Sessional/January 2023 CSE314 Operating System Sessional/xv6/xv6-riscv"
 git clean -fdx
 git reset --hard
-git apply --whitespace=fix "/media/sabah/269CFD019CFCCC75/Level 3 Term 2/Sessional/January 2023 CSE314 Operating System Sessional/Offline 3/1905118.patch"
+git apply --whitespace=fix "/media/sabah/269CFD019CFCCC75/Level 3 Term 2/Sessional/January 2023 CSE314 Operating System Sessional/Offline 5/1905118.patch"
